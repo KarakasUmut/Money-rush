0fe0038983c63f2801564972709d95213fc3094b
KarakasUmut <ukarakas480@gmail.com>
2024-07-21 15:39:43 +0300

    Money-rush

---
+++ Money Rush/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    [SerializeField] float horSpeed;
+    float hor;
+    
+    void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(hor * horSpeed*Time.deltaTime,0,movementSpeed*Time.deltaTime));
+    }
+}
